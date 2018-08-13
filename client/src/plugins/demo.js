b5918cfe1afc1ee8374df1f102fbf0fb220ec725
lushijie <lushijie03@meituan.com>
2018-08-13 17:15:10 +0800

    部分引入

--- client/src/plugins/demo.js
+++ client/src/plugins/demo.js
@@ -2,14 +2,13 @@
 * @Author: lushijie
 * @Date:   2018-06-08 17:47:40
 * @Last Modified by:   lushijie
-* @Last Modified time: 2018-08-13 17:10:39
+* @Last Modified time: 2018-08-13 17:15:01
 */
 import Vue from 'vue';
-// import ElementUI from 'element-ui';
-//
 import { Button, Select } from 'element-ui';
-// import 'element-ui/lib/theme-chalk/index.css';
 
+// import ElementUI from 'element-ui';
+// import 'element-ui/lib/theme-chalk/index.css';
 // Vue.use(ElementUI, { size: 'small' });
 
 Vue.prototype.$ELEMENT = { size: 'small', zIndex: 3000 };
